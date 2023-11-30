a30b183160670d69189b4fe7cf9dc8bb5ce3060e
cxhello <1061594797@qq.com>
2023-11-30 11:34:43 +0800

    update mybatis-generator-config.xml.

---
+++ src/main/java/top/cxhello/generator/config/CustomCommentGenerator.java
@@ -0,0 +1,95 @@
+package top.cxhello.generator.config;
+
+import org.mybatis.generator.api.CommentGenerator;
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.dom.java.*;
+import org.mybatis.generator.api.dom.xml.XmlElement;
+
+import java.util.Properties;
+import java.util.Set;
+
+/**
+ * @author cxhello
+ * @date 2023/11/30
+ */
+public class CustomCommentGenerator implements CommentGenerator {
+
+
+    public void addConfigurationProperties(Properties properties) {
+
+    }
+
+    public void addFieldComment(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+        // 添加字段注释
+        if (introspectedColumn.getRemarks() != null) {
+            field.addJavaDocLine("/** ");
+            field.addJavaDocLine(" * " + introspectedColumn.getRemarks());
+            field.addJavaDocLine(" */");
+        }
+    }
+
+    public void addFieldComment(Field field, IntrospectedTable introspectedTable) {
+
+    }
+
+    public void addModelClassComment(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+
+    }
+
+    public void addClassComment(InnerClass innerClass, IntrospectedTable introspectedTable) {
+
+    }
+
+    public void addClassComment(InnerClass innerClass, IntrospectedTable introspectedTable, boolean b) {
+
+    }
+
+    public void addEnumComment(InnerEnum innerEnum, IntrospectedTable introspectedTable) {
+
+    }
+
+    public void addGetterComment(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+
+    }
+
+    public void addSetterComment(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+
+    }
+
+    public void addGeneralMethodComment(Method method, IntrospectedTable introspectedTable) {
+
+    }
+
+    public void addJavaFileComment(CompilationUnit compilationUnit) {
+
+    }
+
+    public void addComment(XmlElement xmlElement) {
+
+    }
+
+    public void addRootComment(XmlElement xmlElement) {
+
+    }
+
+    public void addGeneralMethodAnnotation(Method method, IntrospectedTable introspectedTable, Set<FullyQualifiedJavaType> set) {
+
+    }
+
+    public void addGeneralMethodAnnotation(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn, Set<FullyQualifiedJavaType> set) {
+
+    }
+
+    public void addFieldAnnotation(Field field, IntrospectedTable introspectedTable, Set<FullyQualifiedJavaType> set) {
+
+    }
+
+    public void addFieldAnnotation(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn, Set<FullyQualifiedJavaType> set) {
+
+    }
+
+    public void addClassAnnotation(InnerClass innerClass, IntrospectedTable introspectedTable, Set<FullyQualifiedJavaType> set) {
+
+    }
+}
